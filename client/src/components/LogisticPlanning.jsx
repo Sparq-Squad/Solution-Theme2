bfd8e6563d52257f88740f79929fb3460229f219
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-06-28 16:46:56 +0530

    add Logistic Planning Component

---
+++ client/src/components/LogisticPlanning.jsx
@@ -0,0 +1,10 @@
+const LogisticPlanning = () => {
+
+    return (
+        <div className="mx-auto my-8 bg-white rounded-lg border border-gray-300 overflow-hidden transition-all duration-300 min-h-full p-4">
+            <h1 className="text-2xl font-bold text-black/75">Logistic Planning</h1>
+        </div>
+    );
+};
+
+export default LogisticPlanning;
