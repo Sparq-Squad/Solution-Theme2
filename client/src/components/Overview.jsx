81693bed33452e83cc10fe9dd836ccb37891d2fc
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-06-28 16:47:39 +0530

    add Overview Component

---
+++ client/src/components/Overview.jsx
@@ -0,0 +1,10 @@
+const Overview = () => {
+
+    return (
+        <div className="mx-auto my-8 bg-white rounded-lg border border-gray-300 overflow-hidden transition-all duration-300 min-h-full p-4">
+            <h1 className="text-2xl font-bold text-black/75">Overview</h1>
+        </div>
+    );
+};
+
+export default Overview;
