ba0bd764920095948aa3bd6744027e95c5842874
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-07-04 16:00:52 +0530

    updated some pages

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,10 +1,13 @@
+// 
+
+import React from 'react'
+
 const Overview = () => {
+  return (
+    <div>
+      <h1>Overview</h1>
+    </div>
+  )
+}
 
-    return (
-        <div className="mx-auto my-8 bg-white rounded-lg border border-gray-300 overflow-hidden transition-all duration-300 min-h-full p-4">
-            <h1 className="text-2xl font-bold text-black/75">Overview</h1>
-        </div>
-    );
-};
-
-export default Overview;
+export default Overview
