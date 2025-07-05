c4653b3220b73b44254cd69b9aea81b14715e697
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-07-05 08:54:04 +0530

    update email utils

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,28 +1,29 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const sendEmail=async(option)=>{
-    try{
+const sendEmail = async (option) => {
+  try {
     //CREATE A TRANSPORTER
-    const transporter=nodemailer.createTransport({
-       host:process.env.EMAIL_HOST,
-       port:process.env.EMAIL_PORT || 587,
-       auth:{
-          user:process.env.EMAIL_USER,
-          pass:process.env.EMAIL_PASSWORD
-       } 
-    })
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT || 587,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
     //DEFINE THE EMAIL OPTIONS
-    const mailOptions={
-        from:"AI BUSINESS ANALYST  support<pminsights123@gmail.com>",
-        to:option.email,
-        subject:option.subject,
-        text:option.message
-    }
-  await transporter.sendMail(mailOptions);
-}catch (err) {
-    console.error('Email sending failed:', err.message);
+    const mailOptions = {
+      from: "AI BUSINESS ANALYST  support<pminsights123@gmail.com>",
+      to: option.email,
+      subject: option.subject,
+      text: option.message,
+    };
+    await transporter.sendMail(mailOptions);
+  } catch (err) {
+    console.error("Email sending failed:", err.message);
     throw err;
   }
 };
 
-module.exports=sendEmail;
+module.exports = sendEmail;
